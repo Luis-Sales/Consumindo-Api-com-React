fe33e3365a7c7bf2a83334ee771e3e254a5dd161
Luis Sales <luissales2608@gmail.com>
2023-02-04 22:09:15 -0300

    sass

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,8 @@ import React from "react"
 
 import { Link } from "react-router-dom"
  
-  
+
+
 const Card = () =>{
     return(
         
@@ -20,7 +21,7 @@ const Card = () =>{
                 
             </div>
         </div>
-    )
+    ) 
 }
 
 export default Card
